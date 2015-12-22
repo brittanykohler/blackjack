453ebc32bf70c408b9175885655e947be42e7065
Jennie Buechner <janina.buechner@gmail.com>
2015-12-21 16:05:41 -0800

    complete game

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,3 +6,6 @@ require "./lib/blackjack/board"
 require "./lib/blackjack/player"
 require "./lib/blackjack/dealer"
 require "./lib/blackjack/game"
+
+game = Blackjack::Game.new
+game.play_game
--- lib/blackjack/game.rb
+++ lib/blackjack/game.rb
@@ -13,14 +13,34 @@ module Blackjack
       player.hand.draw(@deck, 2)
     end
 
+    def declare_winner
+      if @player.get_points > @dealer.get_points
+        return "Player"
+      elsif @player.get_points < @dealer.get_points
+        return "Dealer"
+      else
+        return "Tie"
+      end
+    end
+
     def play_game
       @dealer.deal_hand(@dealer)
       @dealer.deal_hand(@player)
-      if @dealer.blackjack?
-        puts "Dealer had blackjack! You lose :("
+      if @dealer.blackjack? && @player.blackjack?
+        return puts "Both players got blackjack. What are the chances!?"
+      elsif @dealer.blackjack?
+        return puts "Dealer had blackjack! You lose :("
       elsif @player.blackjack?
-        puts "You had blackjack! You win! :)"
+        return puts "You had blackjack! You win! :)"
       end
+      while !@player.stand_status && !@player.bust?
+        @player.make_move
+      end
+      return puts "The player busted. Dealer wins" if @player.bust?
+      while !@dealer.stand_status && !@dealer.bust?
+        @dealer.make_move
+      end
+      return puts "The winner is: #{declare_winner}"
     end
   end
 end
